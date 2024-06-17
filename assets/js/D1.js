74207d17b85907f77c02d816b9f2f63cdf1255ff
Pietroro98 <pietroro98@gmail.com>
2024-06-17 16:31:21 +0200

    settimana2giorno1

--- assets/js/D1.js
+++ assets/js/D1.js
@@ -1,3 +1,4 @@
+//document.getElementById('titolo').innerText = 'Hello WOrld';//
 /*
 REGOLE
 - Tutte le risposte devono essere scritte in JavaScript
@@ -11,38 +12,57 @@ REGOLE
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* I principaly datatype sono:
+- Dato stringa: indica un carattere, ad esempio: const testo='Ciao a tutti'
+
+- Dato numerico: indica il numero specifico e se decimale si esprime sempre con il punto(10.33) esempio: const numero=123456.12 
+
+- dato booleano: il quale verifica se una condizione è vera o è falsa, esempio: const veroOFalso= true /false 
+*/
 
 /* ESERCIZIO 2
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+ /*string myName='Pietro'; 
+ console.log('Pietro');*/
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* let numero1 = 12
+let numero2 = 20
+let numero += numero */
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* let x=12; */
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* let myName='Pietro';
+myName= 'Romano';
+
+const myName= 'pietro';
+myName = 'romano';
+console.log (myName);
+*/
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* let x=12;
+let numero3=4;
+
+ x -= numero3; 
+ */
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -50,4 +70,11 @@ REGOLE
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+/* let name1 = 'john';
+let name2 = 'John';
+
+const verify= name1 !== name2;
+console.log(verify);
+
+let areEqualLowercase = name1.toLowerCase() === name2.toLowerCase();
+console.log(areEqualLowercase);*/
