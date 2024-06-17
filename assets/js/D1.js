6cb2b2b0cff0eb8bf6128fd3fef0562614c26355
Pietroro98 <pietroro98@gmail.com>
2024-06-17 18:01:25 +0200

    settimana2giorno1

--- assets/js/D1.js
+++ assets/js/D1.js
@@ -13,56 +13,62 @@ REGOLE
 */
 
 /* I principaly datatype sono:
-- Dato stringa: indica un carattere, ad esempio: const testo='Ciao a tutti'
+- Dato stringa: indica un carattere, ad esempio: const testo='Ciao a tutti', scritto con le doppie virgolette o con gli apici( ' ' )
 
-- Dato numerico: indica il numero specifico e se decimale si esprime sempre con il punto(10.33) esempio: const numero=123456.12 
+- Dato numerico: indica il numero specifico intero o e se decimale si esprime sempre con il punto(10.33) esempio: const numero=123456.12 
 
 - dato booleano: il quale verifica se una condizione è vera o è falsa, esempio: const veroOFalso= true /false 
+ 
+-dato nullo: variabile senza un valore
+
+dato indefinito: variabile senza un valore, che si aquisirà in seguito.
+
 */
 
 /* ESERCIZIO 2
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
- /*string myName='Pietro'; 
- console.log('Pietro');*/
+ let myName='Pietro'; 
+ console.log(myName);
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-/* let numero1 = 12
-let numero2 = 20
-let numero += numero */
+let num1 = 12
+let num2 = 20
+let sum2 = num1 + num2;
+console.log(sum2);
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
-/* let x=12; */
+let x = 12;
+console.log(x);
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
 
-/* let myName='Pietro';
-myName= 'Romano';
 
-const myName= 'pietro';
-myName = 'romano';
-console.log (myName);
-*/
+myName = 'Romano';
+console.log(myName);
+
+// const -//
+const myConst = 'Pietro';
+console.log(myConst); 
+// myConst = 'romano';// scritto cosi genera un errore e non va avanti
+
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
-
-/* let x=12;
-let numero3=4;
-
- x -= numero3; 
- */
+ let num3 = 4;
+ let sottrazione = x - num3;
+ console.log(sottrazione);
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -70,11 +76,17 @@ let numero3=4;
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
 
-/* let name1 = 'john';
+let name1 = 'john';
 let name2 = 'John';
 
-const verify= name1 !== name2;
+const verify = name1 !== name2 ? 'name 1 è diverso da name 2' : 'name1 è identico a name2';
 console.log(verify);
 
-let areEqualLowercase = name1.toLowerCase() === name2.toLowerCase();
+/*let areEqualLowercase = name1.toLowerCase() === name2.toLowerCase();
 console.log(areEqualLowercase);*/
+
+name1 = name1.toLowerCase;
+name2 = name2.toLowerCase;
+
+const verify2 = name1 !== name2 ? 'name1 è diverso da name2' : 'name1 è identico a name2';
+console.log(verify2);
